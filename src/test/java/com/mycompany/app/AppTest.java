321adc8c92a8d374a0688ce92180932366dbe2be
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2022-11-08 15:33:43 -0300

    Project setup

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,28 @@
+package com.mycompany.app;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void example1() {
+		Assert.assertEquals(2, 3, "This test should be failed");
+	}
+	
+	@Test
+	public void example2() {
+		Assert.assertEquals(2, 2);
+	}
+	
+	@Test
+	public void example3() {
+		Assert.assertTrue(true, "This test should be failed");
+	}
+	
+	@Test
+	    public void example4() {
+		Assert.fail("This test should be failed");
+	    }
+
+}
